928f1d0c4d62a20d6a19ea2654d190fb03eac658
Diogo Oliveira <110493988+diogooliveirabarros@users.noreply.github.com>
2024-06-24 19:44:28 -0300

    Create HelloWorld.c

---
+++ aula 1.1/HelloWorld.c
@@ -0,0 +1,10 @@
+#include <stdio.h> //biblioteca que tem o printf
+#include <stdlib.h>	//biblioteca que tem o system("pause")
+
+int main(){
+	
+	printf("Hello, world!\n");
+	
+	system("pause");
+	
+}
